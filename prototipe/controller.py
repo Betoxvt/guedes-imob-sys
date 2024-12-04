3ed0d496abb23f6c621fb73acc476154d675720e
Roberto German Guedes Neto <robertoguedes@edu.univali.br>
2024-12-03 21:02:18 -0300

    chore: create_tables at controller

---
+++ prototipe/controller.py
@@ -0,0 +1,8 @@
+from db import Base, engine, SessionLocal
+from models import Aluguel, Apartamento, DespesaFixa, Garagem, GastoVariavel, Proprietario
+from schemas import AluguelSchema, ApartamentoSchema, DespesaFixaSchema, GaragemSchema, GastoVariavelSchema, ProprietarioSchema
+
+# Criar as tabelas no banco
+def create_tables():
+    Base.metadata.create_all(engine)
+    print("Tabelas criadas com sucesso!")
