f400cc678a2df19b476bc993e0849107be5200e7
Roberto German Guedes Neto <robertoguedes@edu.univali.br>
2025-01-09 16:47:31 -0300

    update schemas

--- backend/schemas.py
+++ backend/schemas.py
@@ -162,3 +162,75 @@ class ProprietarioResponse(ProprietarioBase):
 
     class Config:
         from_attributes = True
+
+
+# Update schemas
+
+class AluguelUpdate(BaseModel):
+    apartamento_id: Optional[PositiveInt]
+    checkin: Optional[datetime]
+    checkout: Optional[datetime]
+    diarias: Optional[PositiveInt]
+    valor_diaria: Optional[PositiveFloat]
+    taxa_adm: Optional[PositiveFloat]
+    valor_total: Optional[PositiveFloat]
+    valor_imob: Optional[PositiveFloat]
+    valor_prop: Optional[PositiveFloat]
+
+
+class ApartamentoUpdate(BaseModel):
+    apartamento: Optional[str]
+    edificio_id: Optional[PositiveInt]
+    proprietario_id: Optional[PositiveInt]
+    celesc: Optional[PositiveInt]
+    supergasbras: Optional[PositiveInt]
+    internet_provedor: Optional[str]
+    wifiid: Optional[str]
+    wifipass: Optional[str]
+    lockpass: Optional[int]
+
+
+class DespesaUpdate(BaseModel):
+    apartamento_id: Optional[str]
+    data_pagamento: Optional[datetime]
+    valor: Optional[PositiveFloat]
+    descricao: Optional[str]
+
+
+class EdificioUpdate(BaseModel):
+    nome: Optional[str]
+    endereco: Optional[str]
+    numero: Optional[PositiveInt]
+    bairro: Optional[str]
+    cidade: Optional[str]
+    uf: Optional[str]
+    cep: Optional[PositiveInt]
+
+
+class GaragemUpdate(BaseModel):
+    apto_origem_id: Optional[str]
+    apto_destino_id: Optional[str]
+    checkin: Optional[datetime]
+    checkout: Optional[datetime]
+    diarias: Optional[PositiveInt]
+    valor_diaria: Optional[PositiveFloat]
+    taxa_adm: Optional[PositiveFloat]
+    valor_total: Optional[PositiveFloat]
+    valor_imob: Optional[PositiveFloat]
+    valor_prop: Optional[PositiveFloat]
+
+
+class GastoUpdate(BaseModel):
+    apartamento_id: Optional[str]
+    data_pagamento: Optional[datetime]
+    valor_material: Optional[PositiveFloat]
+    valor_mo: Optional[PositiveFloat]
+    valor_total: Optional[PositiveFloat]
+    descricao: Optional[str]
+
+
+class ProprietarioUpdate(BaseModel):
+    nome: Optional[str]
+    cpf: Optional[PositiveInt]
+    telefone: Optional[int]
+    email: Optional[EmailStr]
