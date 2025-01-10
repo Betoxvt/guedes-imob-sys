3bfab78554c275d01995170e7f539e9c353f46d5
Roberto German Guedes Neto <robertoguedes@edu.univali.br>
2025-01-10 13:33:23 -0300

    task 1 done

--- backend/models.py
+++ backend/models.py
@@ -1,25 +1,27 @@
 from sqlalchemy import (
-    Column, String, Integer, BigInteger, Numeric, ForeignKey, Date, DateTime, Text
+    Column, String, Integer, BigInteger, Numeric, ForeignKey, Date, Text
 )
 from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
 from sqlalchemy.schema import CheckConstraint, UniqueConstraint
 from database import Base
 
-# Rentals table
+
 class Aluguel(Base):
     __tablename__ = 'alugueis'
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True)
     apartamento_id = Column(Integer, ForeignKey('apartamentos.id'), nullable=False)
-    checkin = Column(Date, nullable=False)
-    checkout = Column(Date, nullable=False)
+    checkin = Column(Date(timezone=True), nullable=False)
+    checkout = Column(Date(timezone=True), nullable=False)
     diarias = Column(Integer, nullable=False)
     valor_diaria = Column(Numeric(10, 2), nullable=False)
     taxa_adm = Column(Numeric(5, 2), nullable=False)
     valor_total = Column(Numeric(10, 2), nullable=False)
     valor_imob = Column(Numeric(10, 2), nullable=False)
     valor_prop = Column(Numeric(10, 2), nullable=False)
+    criado_em = Column(Date(timezone=True), server_default=func.now(), nullable=False)
+    modificado_em = Column(Date(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False)
 
     # Constraints
     __table_args__ = (
@@ -28,11 +30,11 @@ class Aluguel(Base):
         CheckConstraint('valor_total >= 0', name='check_valor_total_nonnegative'),
     )
 
-# Apartments table
+
 class Apartamento(Base):
     __tablename__ = 'apartamentos'
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True)
     apartamento = Column(String(10), nullable=False)
     edificio_id = Column(Integer, ForeignKey('edificios.id'),nullable=False)
     proprietario_id = Column(Integer, ForeignKey('proprietarios.id'), nullable=False)
@@ -42,29 +44,31 @@ class Apartamento(Base):
     wifiid = Column(Text)
     wifipass = Column(Text)
     lockpass = Column(Integer)
-
+    criado_em = Column(Date(timezone=True), server_default=func.now(), nullable=False)
+    modificado_em = Column(Date(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False)
 
     # Constraints
     __table_args__ = (
         UniqueConstraint('apartamento', 'edificio_id', name='unique_apartamento_edificio'),
     )
     
-# Recurrent expenditure table
+
 class Despesa(Base):
     __tablename__ = 'despesas'
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True)
     apartamento_id = Column(Integer, ForeignKey('apartamentos.id'))
-    data_pagamento = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    data_pagamento = Column(Date(timezone=True), nullable=False)
     valor = Column(Numeric(10, 2), nullable=False)
     descricao = Column(Text, nullable=False)
+    criado_em = Column(Date(timezone=True), server_default=func.now(), nullable=False)
+    modificado_em = Column(Date(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False)
 
 
-# Building address table
 class Edificio(Base):
     __tablename__ = 'edificios'
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True)
     nome = Column(Text, nullable=False)
     logradouro = Column(Text)
     numero = Column(Integer)
@@ -73,6 +77,8 @@ class Edificio(Base):
     uf = Column(String(2))
     pais = Column(Text)
     cep = Column(Integer)
+    criado_em = Column(Date(timezone=True), server_default=func.now(), nullable=False)
+    modificado_em = Column(Date(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False)
 
     # Constraints
     __table_args__ = (
@@ -80,46 +86,50 @@ class Edificio(Base):
     )
 
 
-# Parking rentals table
 class Garagem(Base):
     __tablename__ = 'garagens'
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True)
     apto_origem_id = Column(Integer, ForeignKey('apartamentos.id'), nullable=False)
     apto_destino_id = Column(Integer, ForeignKey('apartamentos.id'), nullable=False)
-    checkin = Column(Date, nullable=False)
-    checkout = Column(Date, nullable=False)
+    checkin = Column(Date(timezone=True), nullable=False)
+    checkout = Column(Date(timezone=True), nullable=False)
     diarias = Column(Integer, nullable=False)
     valor_diaria = Column(Numeric(10, 2), nullable=False)
     taxa_adm = Column(Numeric(5, 2), nullable=False)
     valor_total = Column(Numeric(10, 2), nullable=False)
     valor_imob = Column(Numeric(10, 2), nullable=False)
     valor_prop = Column(Numeric(10, 2), nullable=False)
+    criado_em = Column(Date(timezone=True), server_default=func.now(), nullable=False)
+    modificado_em = Column(Date(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False)
 
     # Constraints
     __table_args__ = (
             CheckConstraint('apto_origem_id != apto_destino_id', name='check_apto_ids_not_equal'),
         )
 
-# Non-Recurrent expenses table
+
 class Gasto(Base):
     __tablename__ = 'gastos'
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True)
     apartamento_id = Column(Integer, ForeignKey('apartamentos.id'), nullable=False)
-    data_pagamento = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    data_pagamento = Column(Date(timezone=True), nullable=False)
     valor_material = Column(Numeric(10, 2))
     valor_mo = Column(Numeric(10, 2))
     valor_total = Column(Numeric(10, 2), nullable=False)
     descricao = Column(Text, nullable=False)
+    criado_em = Column(Date(timezone=True), server_default=func.now(), nullable=False)
+    modificado_em = Column(Date(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False)
 
 
-# Owners table
 class Proprietario(Base):
     __tablename__ = 'proprietarios'
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True)
     nome = Column(Text, nullable=False)
     cpf = Column(BigInteger, unique=True)
     telefone = Column(BigInteger, unique=True)
     email = Column(Text, unique=True)
+    criado_em = Column(Date(timezone=True), server_default=func.now(), nullable=False)
+    modificado_em = Column(Date(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False)
