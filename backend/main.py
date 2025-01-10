6e1e079b97dcfb1cd5112c8a01d07f6f7da252c4
Roberto German Guedes Neto <robertoguedes@edu.univali.br>
2025-01-09 22:12:45 -0300

    FastAPI descriptions

--- backend/main.py
+++ backend/main.py
@@ -5,5 +5,17 @@ from routes import router
 
 models.Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
+app = FastAPI(
+    title="Sistema de Gestão Imobiliária",
+    description="API para gerenciar apartamentos, despesas e aluguéis.",
+    version="1.0.0",
+    contact={
+        "name": "Roberto German Guedes Neto",
+        "email": "robertoguedes@edu.univali.br",
+    },
+    license_info={
+        "name": "MIT",
+        "url": "https://opensource.org/licenses/MIT",
+    },
+)
 app.include_router(router)
