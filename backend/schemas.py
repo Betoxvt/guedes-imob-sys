f4a4abbaf7c5a663939c2696a8209422b82ab738
Roberto German Guedes Neto <robertoguedes@edu.univali.br>
2025-01-11 23:15:29 -0300

    nonnegative

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, PositiveFloat, EmailStr, PositiveInt
+from pydantic import BaseModel, PositiveFloat, EmailStr, PositiveInt, NonNegativeInt, NonNegativeFloat
 from datetime import date
 from typing import Any, Optional
 
@@ -10,41 +10,41 @@ class AluguelBase(BaseModel):
     checkin: date
     checkout: date
     diarias: PositiveInt
-    valor_diaria: Optional[PositiveFloat]
-    taxa_adm: PositiveFloat
-    valor_total: PositiveFloat
-    valor_imob: PositiveFloat
-    valor_prop: PositiveFloat
+    valor_diaria: Optional[NonNegativeInt]
+    taxa_adm: NonNegativeFloat
+    valor_total: NonNegativeFloat
+    valor_imob: NonNegativeFloat
+    valor_prop: NonNegativeFloat
 
 
 class ApartamentoBase(BaseModel):
     apartamento: str
     edificio_id: PositiveInt
     proprietario_id: PositiveInt
-    celesc: Optional[int]
-    supergasbras: Optional[int]
+    celesc: Optional[NonNegativeInt]
+    supergasbras: Optional[NonNegativeInt]
     internet_provedor: Optional[str]
     wifiid: Optional[str]
     wifipass: Optional[str]
-    lockpass: Optional[int]
+    lockpass: Optional[NonNegativeInt]
 
 
 class DespesaBase(BaseModel):
     apartamento_id: PositiveInt
     data_pagamento: date
-    valor: PositiveFloat
+    valor: NonNegativeFloat
     descricao: str
 
 
 class EdificioBase(BaseModel):
     nome: str
     logradouro: Optional[str]
-    numero: Optional[int]
+    numero: Optional[NonNegativeInt]
     bairro: Optional[str]
     cidade: Optional[str]
     uf: Optional[str]
     pais: Optional[str]
-    cep: Optional[int]
+    cep: Optional[NonNegativeInt]
 
 
 class GaragemBase(BaseModel):
@@ -53,26 +53,26 @@ class GaragemBase(BaseModel):
     checkin: date
     checkout: date
     diarias: PositiveInt
-    valor_diaria: Optional[PositiveFloat]
-    taxa_adm: PositiveFloat
-    valor_total: PositiveFloat
-    valor_imob: PositiveFloat
-    valor_prop: PositiveFloat
+    valor_diaria: Optional[NonNegativeInt]
+    taxa_adm: NonNegativeFloat
+    valor_total: NonNegativeFloat
+    valor_imob: NonNegativeFloat
+    valor_prop: NonNegativeFloat
 
 
 class GastoBase(BaseModel):
     apartamento_id: PositiveInt
     data_pagamento: date
-    valor_material: Optional[PositiveFloat]
-    valor_mo: Optional[PositiveFloat]
-    valor_total: PositiveFloat
+    valor_material: Optional[NonNegativeFloat]
+    valor_mo: Optional[NonNegativeFloat]
+    valor_total: NonNegativeFloat
     descricao: str
 
 
 class ProprietarioBase(BaseModel):
     nome: str
-    cpf: Optional[int]
-    telefone: Optional[int]
+    cpf: Optional[NonNegativeInt]
+    telefone: Optional[NonNegativeInt]
     email: Optional[EmailStr]
 
 
@@ -81,14 +81,14 @@ class InquilinoBase(BaseModel):
     nome: str
     tipo_residencia: str
     cidade: str
-    cep: PositiveInt
+    cep: NonNegativeInt
     estado: str
     pais: str
-    telefone: PositiveInt
+    telefone: NonNegativeInt
     estado_civil: str
     profissao: str
-    rg: Optional[PositiveInt]
-    cpf: PositiveInt
+    rg: Optional[NonNegativeInt]
+    cpf: NonNegativeInt
     mae: str
     automovel: Optional[str]
     modelo_auto: Optional[str]
@@ -98,56 +98,56 @@ class InquilinoBase(BaseModel):
     checkout: date
     observacoes: Optional[str]
     proprietario: Optional[str]
-    imob_fone: PositiveInt
+    imob_fone: NonNegativeInt
     acomp_01_nome: Optional[str]
-    acomp_01_rg: Optional[PositiveInt]
-    acomp_01_cpf: Optional[PositiveInt]
-    acomp_01_idade: Optional[int]
+    acomp_01_rg: Optional[NonNegativeInt]
+    acomp_01_cpf: Optional[NonNegativeInt]
+    acomp_01_idade: Optional[NonNegativeInt]
     acomp_01_parentesco: Optional[str]
     acomp_02_nome: Optional[str]
-    acomp_02_rg: Optional[PositiveInt]
-    acomp_02_cpf: Optional[PositiveInt]
-    acomp_02_idade: Optional[int]
+    acomp_02_rg: Optional[NonNegativeInt]
+    acomp_02_cpf: Optional[NonNegativeInt]
+    acomp_02_idade: Optional[NonNegativeInt]
     acomp_02_parentesco: Optional[str]
     acomp_03_nome: Optional[str]
-    acomp_03_rg: Optional[PositiveInt]
-    acomp_03_cpf: Optional[PositiveInt]
-    acomp_03_idade: Optional[int]
+    acomp_03_rg: Optional[NonNegativeInt]
+    acomp_03_cpf: Optional[NonNegativeInt]
+    acomp_03_idade: Optional[NonNegativeInt]
     acomp_03_parentesco: Optional[str]
     acomp_04_nome: Optional[str]
-    acomp_04_rg: Optional[PositiveInt]
-    acomp_04_cpf: Optional[PositiveInt]
-    acomp_04_idade: Optional[int]
+    acomp_04_rg: Optional[NonNegativeInt]
+    acomp_04_cpf: Optional[NonNegativeInt]
+    acomp_04_idade: Optional[NonNegativeInt]
     acomp_04_parentesco: Optional[str]
     acomp_05_nome: Optional[str]
-    acomp_05_rg: Optional[PositiveInt]
-    acomp_05_cpf: Optional[PositiveInt]
-    acomp_05_idade: Optional[int]
+    acomp_05_rg: Optional[NonNegativeInt]
+    acomp_05_cpf: Optional[NonNegativeInt]
+    acomp_05_idade: Optional[NonNegativeInt]
     acomp_05_parentesco: Optional[str]
     acomp_06_nome: Optional[str]
-    acomp_06_rg: Optional[PositiveInt]
-    acomp_06_cpf: Optional[PositiveInt]
-    acomp_06_idade: Optional[int]
+    acomp_06_rg: Optional[NonNegativeInt]
+    acomp_06_cpf: Optional[NonNegativeInt]
+    acomp_06_idade: Optional[NonNegativeInt]
     acomp_06_parentesco: Optional[str]
     acomp_07_nome: Optional[str]    
-    acomp_07_rg: Optional[PositiveInt]
-    acomp_07_cpf: Optional[PositiveInt]
-    acomp_07_idade: Optional[int]
+    acomp_07_rg: Optional[NonNegativeInt]
+    acomp_07_cpf: Optional[NonNegativeInt]
+    acomp_07_idade: Optional[NonNegativeInt]
     acomp_07_parentesco: Optional[str]
     acomp_08_nome: Optional[str]
-    acomp_08_rg: Optional[PositiveInt]
-    acomp_08_cpf: Optional[PositiveInt]
-    acomp_08_idade: Optional[int]
+    acomp_08_rg: Optional[NonNegativeInt]
+    acomp_08_cpf: Optional[NonNegativeInt]
+    acomp_08_idade: Optional[NonNegativeInt]
     acomp_08_parentesco: Optional[str]
     acomp_09_nome: Optional[str]
-    acomp_09_rg: Optional[PositiveInt]
-    acomp_09_cpf: Optional[PositiveInt]
-    acomp_09_idade: Optional[int]
+    acomp_09_rg: Optional[NonNegativeInt]
+    acomp_09_cpf: Optional[NonNegativeInt]
+    acomp_09_idade: Optional[NonNegativeInt]
     acomp_09_parentesco: Optional[str]
     acomp_10_nome: Optional[str]
-    acomp_10_rg: Optional[PositiveInt]
-    acomp_10_cpf: Optional[PositiveInt]
-    acomp_10_idade: Optional[int]
+    acomp_10_rg: Optional[NonNegativeInt]
+    acomp_10_cpf: Optional[NonNegativeInt]
+    acomp_10_idade: Optional[NonNegativeInt]
     acomp_10_parentesco: Optional[str]
 
 
@@ -273,41 +273,41 @@ class AluguelUpdate(BaseModel):
     checkin: Optional[date]
     checkout: Optional[date]
     diarias: Optional[PositiveInt]
-    valor_diaria: Optional[PositiveFloat]
-    taxa_adm: Optional[PositiveFloat]
-    valor_total: Optional[PositiveFloat]
-    valor_imob: Optional[PositiveFloat]
-    valor_prop: Optional[PositiveFloat]
+    valor_diaria: Optional[NonNegativeInt]
+    taxa_adm: Optional[NonNegativeFloat]
+    valor_total: Optional[NonNegativeFloat]
+    valor_imob: Optional[NonNegativeFloat]
+    valor_prop: Optional[NonNegativeFloat]
 
 
 class ApartamentoUpdate(BaseModel):
     apartamento: Optional[str]
     edificio_id: Optional[PositiveInt]
     proprietario_id: Optional[PositiveInt]
-    celesc: Optional[int]
-    supergasbras: Optional[int]
+    celesc: Optional[NonNegativeInt]
+    supergasbras: Optional[NonNegativeInt]
     internet_provedor: Optional[str]
     wifiid: Optional[str]
     wifipass: Optional[str]
-    lockpass: Optional[int]
+    lockpass: Optional[NonNegativeInt]
 
 
 class DespesaUpdate(BaseModel):
     apartamento_id: Optional[PositiveInt]
     data_pagamento: Optional[date]
-    valor: Optional[PositiveFloat]
+    valor: Optional[NonNegativeFloat]
     descricao: Optional[str]
 
 
 class EdificioUpdate(BaseModel):
     nome: Optional[str]
     logradouro: Optional[str]
-    numero: Optional[int]
+    numero: Optional[NonNegativeInt]
     bairro: Optional[str]
     cidade: Optional[str]
     uf: Optional[str]
     pais: Optional[str]
-    cep: Optional[int]
+    cep: Optional[NonNegativeInt]
 
 
 class GaragemUpdate(BaseModel):
@@ -316,26 +316,26 @@ class GaragemUpdate(BaseModel):
     checkin: Optional[date]
     checkout: Optional[date]
     diarias: Optional[PositiveInt]
-    valor_diaria: Optional[PositiveFloat]
-    taxa_adm: Optional[PositiveFloat]
-    valor_total: Optional[PositiveFloat]
-    valor_imob: Optional[PositiveFloat]
-    valor_prop: Optional[PositiveFloat]
+    valor_diaria: Optional[NonNegativeInt]
+    taxa_adm: Optional[NonNegativeFloat]
+    valor_total: Optional[NonNegativeFloat]
+    valor_imob: Optional[NonNegativeFloat]
+    valor_prop: Optional[NonNegativeFloat]
 
 
 class GastoUpdate(BaseModel):
     apartamento_id: Optional[PositiveInt]
     data_pagamento: Optional[date]
-    valor_material: Optional[PositiveFloat]
-    valor_mo: Optional[PositiveFloat]
-    valor_total: Optional[PositiveFloat]
+    valor_material: Optional[NonNegativeFloat]
+    valor_mo: Optional[NonNegativeFloat]
+    valor_total: Optional[NonNegativeFloat]
     descricao: Optional[str]
 
 
 class ProprietarioUpdate(BaseModel):
     nome: Optional[str]
-    cpf: Optional[int]
-    telefone: Optional[int]
+    cpf: Optional[NonNegativeInt]
+    telefone: Optional[NonNegativeInt]
     email: Optional[EmailStr]
 
 
@@ -344,14 +344,14 @@ class InquilinoUpdate(BaseModel):
     nome: Optional[str]
     tipo_residencia: Optional[str]
     cidade: Optional[str]
-    cep: Optional[PositiveInt]
+    cep: Optional[NonNegativeInt]
     estado: Optional[str]
     pais: Optional[str]
-    telefone: Optional[PositiveInt]
+    telefone: Optional[NonNegativeInt]
     estado_civil: Optional[str]
     profissao: Optional[str]
-    rg: Optional[PositiveInt]
-    cpf: Optional[PositiveInt]
+    rg: Optional[NonNegativeInt]
+    cpf: Optional[NonNegativeInt]
     mae: Optional[str]
     automovel: Optional[str]
     modelo_auto: Optional[str]
@@ -361,54 +361,54 @@ class InquilinoUpdate(BaseModel):
     checkout: Optional[date]
     observacoes: Optional[str]
     proprietario: Optional[str]
-    imob_fone: Optional[PositiveInt]
+    imob_fone: Optional[NonNegativeInt]
     acomp_01_nome: Optional[str]
-    acomp_01_rg: Optional[PositiveInt]
-    acomp_01_cpf: Optional[PositiveInt]
-    acomp_01_idade: Optional[int]
+    acomp_01_rg: Optional[NonNegativeInt]
+    acomp_01_cpf: Optional[NonNegativeInt]
+    acomp_01_idade: Optional[NonNegativeInt]
     acomp_01_parentesco: Optional[str]
     acomp_02_nome: Optional[str]
-    acomp_02_rg: Optional[PositiveInt]
-    acomp_02_cpf: Optional[PositiveInt]
-    acomp_02_idade: Optional[int]
+    acomp_02_rg: Optional[NonNegativeInt]
+    acomp_02_cpf: Optional[NonNegativeInt]
+    acomp_02_idade: Optional[NonNegativeInt]
     acomp_02_parentesco: Optional[str]
     acomp_03_nome: Optional[str]
-    acomp_03_rg: Optional[PositiveInt]
-    acomp_03_cpf: Optional[PositiveInt]
-    acomp_03_idade: Optional[int]
+    acomp_03_rg: Optional[NonNegativeInt]
+    acomp_03_cpf: Optional[NonNegativeInt]
+    acomp_03_idade: Optional[NonNegativeInt]
     acomp_03_parentesco: Optional[str]
     acomp_04_nome: Optional[str]
-    acomp_04_rg: Optional[PositiveInt]
-    acomp_04_cpf: Optional[PositiveInt]
-    acomp_04_idade: Optional[int]
+    acomp_04_rg: Optional[NonNegativeInt]
+    acomp_04_cpf: Optional[NonNegativeInt]
+    acomp_04_idade: Optional[NonNegativeInt]
     acomp_04_parentesco: Optional[str]
     acomp_05_nome: Optional[str]
-    acomp_05_rg: Optional[PositiveInt]
-    acomp_05_cpf: Optional[PositiveInt]
-    acomp_05_idade: Optional[int]
+    acomp_05_rg: Optional[NonNegativeInt]
+    acomp_05_cpf: Optional[NonNegativeInt]
+    acomp_05_idade: Optional[NonNegativeInt]
     acomp_05_parentesco: Optional[str]
     acomp_06_nome: Optional[str]
-    acomp_06_rg: Optional[PositiveInt]
-    acomp_06_cpf: Optional[PositiveInt]
-    acomp_06_idade: Optional[int]
+    acomp_06_rg: Optional[NonNegativeInt]
+    acomp_06_cpf: Optional[NonNegativeInt]
+    acomp_06_idade: Optional[NonNegativeInt]
     acomp_06_parentesco: Optional[str]
     acomp_07_nome: Optional[str]
-    acomp_07_rg: Optional[PositiveInt]
-    acomp_07_cpf: Optional[PositiveInt]
-    acomp_07_idade: Optional[int]
+    acomp_07_rg: Optional[NonNegativeInt]
+    acomp_07_cpf: Optional[NonNegativeInt]
+    acomp_07_idade: Optional[NonNegativeInt]
     acomp_07_parentesco: Optional[str]
     acomp_08_nome: Optional[str]
-    acomp_08_rg: Optional[PositiveInt]
-    acomp_08_cpf: Optional[PositiveInt]
-    acomp_08_idade: Optional[int]
+    acomp_08_rg: Optional[NonNegativeInt]
+    acomp_08_cpf: Optional[NonNegativeInt]
+    acomp_08_idade: Optional[NonNegativeInt]
     acomp_08_parentesco: Optional[str]
     acomp_09_nome: Optional[str]
-    acomp_09_rg: Optional[PositiveInt]
-    acomp_09_cpf: Optional[PositiveInt]
-    acomp_09_idade: Optional[int]
+    acomp_09_rg: Optional[NonNegativeInt]
+    acomp_09_cpf: Optional[NonNegativeInt]
+    acomp_09_idade: Optional[NonNegativeInt]
     acomp_09_parentesco: Optional[str]
     acomp_10_nome: Optional[str]
-    acomp_10_rg: Optional[PositiveInt]
-    acomp_10_cpf: Optional[PositiveInt]
-    acomp_10_idade: Optional[int]
+    acomp_10_rg: Optional[NonNegativeInt]
+    acomp_10_cpf: Optional[NonNegativeInt]
+    acomp_10_idade: Optional[NonNegativeInt]
     acomp_10_parentesco: Optional[str]
