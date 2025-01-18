80896d663a7311aa660a4816169fb76a26e9fa5b
Roberto German Guedes Neto <robertoguedes@edu.univali.br>
2025-01-18 00:46:39 -0300

    all good on the happy path for fill_ficha()

--- frontend/utils/mypdf.py
+++ frontend/utils/mypdf.py
@@ -1,7 +1,8 @@
+from datetime import datetime
 import os
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A4
-from datetime import datetime
+import json
 
 
 def fill_ficha(data: dict[str, str], img: str, dir: str):
@@ -29,13 +30,34 @@ def fill_ficha(data: dict[str, str], img: str, dir: str):
         "cor_auto":     [302, 288.5],
         "proprietario": [195, 744],
     }
+    acomp_coord = [
+        {'doc': [293, 328], 'idade': [405, 328], 'nome': [101, 328], 'parentesco': [443, 328]},
+        {'doc': [293, 344], 'idade': [405, 344], 'nome': [101, 344], 'parentesco': [443, 344]},
+        {'doc': [293, 360], 'idade': [405, 360], 'nome': [101, 360], 'parentesco': [443, 360]},
+        {'doc': [293, 376], 'idade': [405, 376], 'nome': [101, 376], 'parentesco': [443, 376]},
+        {'doc': [293, 392], 'idade': [405, 392], 'nome': [101, 392], 'parentesco': [443, 392]},
+        {'doc': [293, 407], 'idade': [405, 407], 'nome': [101, 407], 'parentesco': [443, 407]},
+        {'doc': [293, 422], 'idade': [405, 422], 'nome': [101, 422], 'parentesco': [443, 422]},
+        {'doc': [293, 438], 'idade': [405, 438], 'nome': [101, 438], 'parentesco': [443, 438]}, 
+        {'doc': [293, 454], 'idade': [405, 454], 'nome': [101, 454], 'parentesco': [443, 454]},
+        {'doc': [293, 470], 'idade': [405, 470], 'nome': [101, 470], 'parentesco': [443, 470]}
+    ]
+    acomps = ['a0', 'a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8', 'a9']
+    counter = 0
     for k, v in data.items():
         for i, coord in s.items():
             if k == i:
-                x: int  = coord[0]
-                y: int = (A4[1] - coord[1])
-                c.drawString(x=x, y=y, text=v)
-    
+                x = coord[0]
+                y = (A4[1] - coord[1])
+                c.drawString(x=x, y=y, text=str(v))
+        if k in acomps and data[k] is not None:
+            field: dict = json.loads(data[k])
+            for key, value in field.items():
+                x, y = acomp_coord[counter][key]
+                v = value
+                c.drawString(x=x, y=A4[1] - y, text=str(v))
+            counter += 1
+
     tipo_residencia = {'anual': [177, 207], 'temp': [253, 207]}
     observacoes = {'l1': [148, 519], 'l2': [86, 531]}
     imob_fone = {'ddd': [360, 708], 'tel': [390, 708]}
@@ -52,33 +74,32 @@ def fill_ficha(data: dict[str, str], img: str, dir: str):
     
     if data['observacoes']:
         x, y = observacoes['l1']
-        c.drawString(x, A4[1]-y, data['observacoes'])
+        c.drawString(x, A4[1]-y, str(data['observacoes']))
 
     if data['imob_fone']:
         x, y = imob_fone['ddd']
-        c.drawString(x, A4[1]-y, data['imob_fone'][5:7])
+        c.drawString(x, A4[1]-y, str(data['imob_fone'][5:7]))
         x, y = imob_fone['tel']
-        c.drawString(x, A4[1]-y, data['imob_fone'][9:])
+        c.drawString(x, A4[1]-y, str(data['imob_fone'][9:]))
 
     checkin = {'dia': [222, 494], 'mes': [262, 494], 'ano': [292, 494]}
     checkout = {'dia': [379, 494], 'mes': [422, 494], 'ano': [457, 494]}
     
     if data['checkin']:
         x, y = checkin['dia']
-        c.drawString(x, A4[1]-y, data['checkin'][8:])
+        c.drawString(x, A4[1]-y, str(data['checkin'][8:]))
         x, y = checkin['mes']
-        c.drawString(x, A4[1]-y, data['checkin'][5:7])
+        c.drawString(x, A4[1]-y, str(data['checkin'][5:7]))
         x, y = checkin['ano']
-        c.drawString(x, A4[1]-y, data['checkin'][:4])
+        c.drawString(x, A4[1]-y, str(data['checkin'][:4]))
 
     if data['checkout']:
         x, y = checkout['dia']
-        c.drawString(x, A4[1]-y, data['checkout'][8:])
+        c.drawString(x, A4[1]-y, str(data['checkout'][8:]))
         x, y = checkout['mes']
-        c.drawString(x, A4[1]-y, data['checkout'][5:7])
+        c.drawString(x, A4[1]-y, str(data['checkout'][5:7]))
         x, y = checkout['ano']
-        c.drawString(x, A4[1]-y, data['checkout'][:4])
-
+        c.drawString(x, A4[1]-y, str(data['checkout'][:4]))
 
     c.save()
     return file_name
@@ -86,7 +107,7 @@ def fill_ficha(data: dict[str, str], img: str, dir: str):
 if __name__ == '__main__':
     import csv
 
-    with open('./files/data_test.csv', 'r', encoding='utf-8-sig') as csvfile:
+    with open('./files/data_test2.csv', 'r', encoding='utf-8-sig') as csvfile:
         reader = csv.reader(csvfile)
         chaves = next(reader)
         valores = next(reader)
@@ -99,13 +120,4 @@ if __name__ == '__main__':
 
 
 
-a0 = {'nome': [101, 329], 'doc': [291, 329], 'idade': [397, 329], 'parentesco': [439, 329]}
-a1 = {'nome': [101, 345], 'doc': [291, 345], 'idade': [397, 345], 'parentesco': [439, 345]}
-a2 = {'nome': [101, 361], 'doc': [291, 361], 'idade': [397, 361], 'parentesco': [439, 361]}
-a3 = {'nome': [101, 377], 'doc': [291, 377], 'idade': [397, 377], 'parentesco': [439, 377]}
-a4 = {'nome': [101, 393], 'doc': [291, 393], 'idade': [397, 393], 'parentesco': [439, 393]}
-a5 = {'nome': [101, 408], 'doc': [291, 408], 'idade': [397, 408], 'parentesco': [439, 408]}
-a6 = {'nome': [101, 423], 'doc': [291, 423], 'idade': [397, 423], 'parentesco': [439, 423]}
-a7 = {'nome': [101, 439], 'doc': [291, 439], 'idade': [397, 439], 'parentesco': [439, 439]}
-a8 = {'nome': [101, 455], 'doc': [291, 455], 'idade': [397, 455], 'parentesco': [439, 455]}
-a9 = {'nome': [101, 471], 'doc': [291, 471], 'idade': [397, 471], 'parentesco': [439, 471]}
+
