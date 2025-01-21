3c7124ed3b5f6a377408096c95b87d888fe1247f
Roberto German Guedes Neto <robertoguedes@edu.univali.br>
2025-01-21 12:14:23 -0300

    add number format functions
    Signed-off-by: Roberto German Guedes Neto <robertoguedes@edu.univali.br>

--- app/utils/mynum.py
+++ app/utils/mynum.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import re
 
 
 def calculate_valortotal(diarias, valor_diaria):
@@ -35,3 +36,64 @@ def calculate_saldo(total, depositado):
         saldo = depositado - total
         st.write(f'Saldo: {saldo}')
         return saldo
+
+
+def cep_input(cep_in: str | int | float) -> str:
+    cep_num = re.sub(r'[^0-9]', '', str(cep_in))
+    if len(cep_num) == 8:
+        return f'{cep_num[:5]}-{cep_num[5:]}'
+    else:
+        st.warning('Não foi identificado o padrão brasileiro')
+        return cep_in
+
+
+def cpf_input(cpf_in: str | int | float) -> str:
+    cpf_num = re.sub(r'[^0-9]', '', str(cpf_in))
+    if len(cpf_num) == 11:
+        return f'{cpf_num[:3]}.{cpf_num[3:6]}.{cpf_num[6:9]}-{cpf_num[9:]}'
+    else:
+        st.warning('Não foi identificado o padrão brasileiro')
+        return cpf_in
+    
+
+def rg_input(rg_in: str | int | float) -> str:
+    rg_num = re.sub(r'[^0-9]', '', str(rg_in))
+    match len(rg_num):
+        case 7:
+            return f'{rg_num[:1]}.{rg_num[1:4]}.{rg_num[4:]}'
+        case 8:
+            if str(rg_in)[:2].isalpha():
+                return f'{rg_in[:2].upper()}-{rg_num[:2]}.{rg_num[2:5]}.{rg_num[5:]}'
+            if len(str(rg_in)) > 8 and str(rg_in)[8:].isalpha():
+                return f'{rg_num[:2]}.{rg_num[2:5]}.{rg_num[5:]}-{rg_in[8:10].upper()}'
+            else:
+                return f'{rg_num[0]}.{rg_num[1:4]}.{rg_num[4:7]}-{rg_num[7]}'
+        case 9:
+            return f'{rg_num[:2]}.{rg_num[2:5]}.{rg_num[5:8]-[8]}'
+        case 11:
+            return f'{rg_num[:11]}-{rg_num[11]}'
+        case _:
+            st.warning('Não foi identificado o padrão brasileiro')
+            return rg_in
+
+
+def tel_input_br(tel_in: str | int | float) -> str:
+    tel_num = re.sub(r'[^0-9]', '', str(tel_in))
+    match len(tel_num):
+        case 8:
+            return f'{tel_num[:4]}-{tel_num[4:]}'
+        case 9:
+            return f'{tel_num[0]} {tel_num[1:5]}-{tel_num[5:]}'
+        case 10:
+            return f'+55 ({tel_num[:2]}) {tel_num[2:6]}-{tel_num[6:]}'
+        case 11:
+            return f'+55 ({tel_num[:2]}) {tel_num[2]} {tel_num[3:7]}-{tel_num[7:]}'
+        case 12:
+            return tel_num
+        case 13:
+            return f'+{tel_num[:2]} ({tel_num[2:4]}) {tel_num[4]} {tel_num[5:9]}-{tel_num[9:]}'
+        case _:
+            st.warning('Não foi identificado o padrão brasileiro')
+            return tel_in
+
+
