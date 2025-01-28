bb5abd961bc9afc504e0e91e38012f018e6ad4d2
Roberto German Guedes Neto <robertoguedes@edu.univali.br>
2025-01-28 13:37:21 -0300

    on_select dataframes
    Signed-off-by: Roberto German Guedes Neto <robertoguedes@edu.univali.br>

--- app/pages/9_Planilha_de_Reservas.py
+++ app/pages/9_Planilha_de_Reservas.py
@@ -1,9 +1,12 @@
 from datetime import date
+import locale
 import pandas as pd
 import requests
 import streamlit as st
-from utils.mydate import gen_reserv_table, showbr_dfdate
+from utils.mydate import gen_reserv_table, showbr_dfdate, str_to_date
 from utils.mystr import apto_input
+
+locale.setlocale(locale.LC_ALL, "pt_BR.UTF-8")
 
 st.set_page_config(page_title="Planilha de Reservas", layout="wide")
 st.title("Planilha de Reservas")
@@ -29,17 +32,57 @@ with tab1:
         key=9101,
     )
     if ano and mes:
+        st.markdown(f"### {mes} / {ano}")
         planilha = gen_reserv_table(ano, mes)
     grandes = planilha.index.str.startswith("A") | planilha.index.str.endswith("0")
     pequenos = ~grandes
-    planilha_g = planilha.loc[grandes]
-    planilha_p = planilha.loc[pequenos]
-    if not planilha_g.empty:
+    if "planilha_g" not in st.session_state:
+        st.session_state.planilha_g = planilha.loc[grandes]
+    if "planilha_p" not in st.session_state:
+        st.session_state.planilha_p = planilha.loc[pequenos]
+    if not st.session_state.planilha_g.empty:
         st.subheader("Apartamentos Grandes")
-        st.dataframe(planilha_g)
-    if not planilha_p.empty:
+        event_g = st.dataframe(
+            st.session_state.planilha_g,
+            selection_mode=["single-column", "single-row"],
+            on_select="rerun",
+            key=91111,
+        )
+        if event_g.selection["rows"] and event_g.selection["columns"]:
+            day_g = event_g.selection["columns"][0]
+            index_apto_g = event_g.selection["rows"][0]
+            apto_g = st.session_state.planilha_g.iloc[index_apto_g].name
+            st.write(
+                f"Apartamento: {apto_g}. Reserva com check-in em: {day_g}/{mes}/{ano}"
+            )
+            alugueis_g_response = requests.get("http://api:8000/alugueis/")
+            if alugueis_g_response.status_code == 200:
+                alugueis_g = alugueis_g_response.json()
+                df_g = pd.DataFrame(alugueis_g)
+                df_g = df_g[df_g["apto_id"] == apto_g]
+                df_g = df_g[
+                    df_g["checkin"] == str_to_date(f"{ano}-{mes}-{day_g}").isoformat()
+                ]
+                if not df_g.empty:
+                    st.write(f"Aluguel em nome de {df_g.iloc[0]['nome']}")
+                    st.write(
+                        f"Valor total: {locale.currency(df_g.iloc[0]['valor_total'])}"
+                    )
+                    pagamentos_g_response = requests.get("http://api:8000/pagamentos/")
+
+    if not st.session_state.planilha_p.empty:
         st.subheader("Apartamentos Pequenos")
-        st.dataframe(planilha_p)
+        event_p = st.dataframe(
+            st.session_state.planilha_p,
+            selection_mode=["single-column", "single-row"],
+            on_select="rerun",
+            key=92222,
+        )
+        if event_p.selection["rows"] and event_p.selection["columns"]:
+            day_p = event_p.selection["columns"][0]
+            index_apto_p = event_p.selection["rows"][0]
+            apto_p = st.session_state.planilha_p.iloc[index_apto_p].name
+            st.write(f"Apto {apto_p}, dia {day_p}")
 
 with tab2:
     st.header("Consultar Reservas | Por Apartamento")
