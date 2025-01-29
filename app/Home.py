6047ae90113adedcf16dcc36ebe4311c5641e5cb
Roberto German Guedes Neto <robertoguedes@edu.univali.br>
2025-01-29 00:08:39 -0300

    quick consult
    Signed-off-by: Roberto German Guedes Neto <robertoguedes@edu.univali.br>

--- app/Home.py
+++ app/Home.py
@@ -1,3 +1,4 @@
+from datetime import date, timedelta
 import streamlit as st
 import streamlit_authenticator as stauth
 import yaml
@@ -8,6 +9,7 @@ from utils.mystr import apto_input
 import requests
 import pandas as pd
 import locale
+import numpy as np
 
 locale.setlocale(locale.LC_ALL, "pt_BR.UTF-8")
 
@@ -32,6 +34,7 @@ if st.session_state["authentication_status"]:
     st.write(f'Usuário: *{st.session_state["name"]}*')
     st.subheader("Consultas Rápidas")
     tab1, tab2 = st.tabs(["Por Apartamento", "Por Datas"])
+
     with tab1:
         apto_id = apto_input(
             st.text_input(label="Apartamento", value=None, max_chars=5, key=10000)
@@ -153,8 +156,102 @@ if st.session_state["authentication_status"]:
                         show_response_message(get_garagens_origem_response)
 
     with tab2:
-        check_day = st.date_input("Data", format="DD/MM/YYYY")
 
+        def color_df(val: str):
+            is_index_col = val.name == "Apto."
+            return pd.Series(
+                np.where(
+                    is_index_col,
+                    "color: white; font-weight: bold",
+                    np.where(
+                        val == "Livre",
+                        "background-color: green",
+                        "background-color: red",
+                    ),
+                ),
+                index=val.index,
+            )
+
+        checkin = st.date_input("Check-in", format="DD/MM/YYYY")
+        checkout = st.date_input("Check-out", format="DD/MM/YYYY", value=None)
+        days = st.number_input("Diarias", step=1, format="%d", value=None)
+
+        if checkout is None and days is not None:
+            checkout = checkin + timedelta(days=days)
+
+        verify_button = st.button("Verificar")
+        if verify_button:
+            aptos_response = requests.get(f"http://api:8000/apartamentos/")
+            if aptos_response.status_code == 200:
+                aptos_data = aptos_response.json()
+                df_aptos = pd.DataFrame(aptos_data, columns=["id"])
+                df_aptos["Situação"] = "Livre"
+                df_aptos["Até"] = None
+                df_aptos = df_aptos.set_index("id")
+            else:
+                show_response_message(aptos_response)
+            alug_response = requests.get(f"http://api:8000/alugueis/")
+            if alug_response.status_code == 200:
+                alug_data = alug_response.json()
+                df_alug = pd.DataFrame(
+                    alug_data, columns=["apto_id", "checkin", "checkout"]
+                )
+                df_alug = df_alug.set_index("apto_id")
+                if not df_alug.empty and not df_aptos.empty:
+                    for index, aluguel in df_alug.iterrows():
+                        checkin_date = date.fromisoformat(aluguel["checkin"])
+                        checkout_date = date.fromisoformat(aluguel["checkout"])
+                        if checkin and checkout:
+                            if not (
+                                checkout_date <= checkin or checkin_date >= checkout
+                            ):
+                                # df_aptos.loc[index, "Situação"] = "Reservado"
+                                df_aptos = df_aptos.drop(index=index)
+                            else:
+                                try:
+                                    if df_aptos.loc[index, "Até"] is None:
+                                        df_aptos.loc[index, "Até"] = checkin_date
+                                    elif checkin_date < df_aptos.loc[index, "Até"]:
+                                        df_aptos.loc[index, "Até"] = checkin_date
+                                except:
+                                    pass
+                        elif checkin and not checkout:
+                            if checkin < checkin_date:
+                                try:
+                                    if df_aptos.loc[index, "Até"] is None:
+                                        df_aptos.loc[index, "Até"] = checkin_date
+                                    elif checkin_date < df_aptos.loc[index, "Até"]:
+                                        df_aptos.loc[index, "Até"] = checkin_date
+                                except:
+                                    pass
+                            else:
+                                try:
+                                    if (
+                                        checkin_date <= checkin
+                                        and checkin < checkout_date
+                                    ):
+                                        # df_aptos.loc[index, "Situação"] = "Reservado"
+                                        df_aptos = df_aptos.drop(index=index)
+                                except:
+                                    pass
+                    df_aptos["Apto."] = df_aptos.index
+                    df_aptos = df_aptos[["Apto.", "Situação", "Até"]]
+                    st.dataframe(
+                        hide_index=True,
+                        data=df_aptos.style.apply(
+                            color_df, subset=["Situação", "Apto."]
+                        ),
+                        column_config={
+                            "Até": st.column_config.DateColumn(
+                                "Até", format="DD/MM/YYYY"
+                            ),
+                        },
+                    )
+
+                else:
+                    st.warning("Não há alugueis e/ou apartamentos registrados")
+            else:
+                show_response_message(alug_response)
 
 elif st.session_state["authentication_status"] is False:
     st.write("Usuário ou senha incorretos")
