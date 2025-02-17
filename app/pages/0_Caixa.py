58ca7000b363282aa33d243a8d6ecd054176a41c
Roberto German Guedes Neto <robertoguedes@edu.univali.br>
2025-02-16 22:18:46 -0300

    adds a complete caixa page
    Signed-off-by: Roberto German Guedes Neto <robertoguedes@edu.univali.br>

--- app/pages/0_Caixa.py
+++ app/pages/0_Caixa.py
@@ -2,7 +2,6 @@ from datetime import date
 import pandas as pd
 import requests
 import streamlit as st
-from utils.mydate import str_to_date
 from utils.myfunc import cat_index, show_data_output, show_response_message
 
 st.set_page_config(page_title="Caixa", layout="wide")
@@ -16,9 +15,7 @@ else:
     st.write(f'Usuário: *{st.session_state["name"]}*')
     authenticator.logout(location="sidebar")
 
-tab1, tab2, tab3, tab4, tab5 = st.tabs(
-    ["Registrar", "Consultar", "Modificar", "Deletar", "listar"]
-)
+tab1, tab2, tab3, tab4 = st.tabs(["Registrar", "Consultar", "Modificar", "Deletar"])
 
 moedas = ["BRL", "USD"]
 
@@ -50,6 +47,7 @@ with tab1:
 
 with tab2:
     st.header("Consultar Caixa")
+    params = {}
 
     with st.expander(label="Filtros"):
         start_date = st.date_input(
@@ -66,7 +64,79 @@ with tab2:
         )
         moeda = st.selectbox(label="Moeda", options=["Todos", "BRL", "USD"])
 
+        params = {
+            "start_date": start_date,
+            "end_date": end_date,
+            "signal": signal,
+            "moeda": moeda,
+        }
+
     if st.button("Consultar"):
-        get_list_response = requests.get(
-            f"http://api:8000/caixa/?start_date={start_date}&end_date={end_date}&signal={signal}&moeda={moeda}"
-        )
+        get_list_response = requests.get(f"http://api:8000/caixa/", params=params)
+        if get_list_response.status_code == 200:
+            lista = get_list_response.json()
+            if lista:
+                df_list = pd.DataFrame(lista)
+
+                st.dataframe(df_list.set_index("id"))
+            else:
+                st.warning("Sem dados, verifque os filtros")
+        else:
+            show_response_message(get_list_response)
+
+with tab3:
+    st.header("Modificar Registro de Caixa")
+    update_id = st.number_input(
+        "ID do Registro de Caixa", min_value=1, value=None, format="%d"
+    )
+    if update_id:
+        update_response = requests.get(f"http//api:8000/caixa/{update_id}")
+        if update_response.status_code == 200:
+            caixa_up = update_response.json()
+            df_up = pd.DataFrame([caixa_up])
+            st.dataframe(df_up.set_index("id"))
+            moeda: str = st.selectbox(
+                label="Moeda",
+                options=moedas,
+                index=cat_index(df_up, "moeda", moedas),
+                key="moeda_up",
+            )
+            valor: float = st.number_input(
+                label="Valor", format="%.2f", value=df_up.loc[0, "valor"]
+            )
+            if st.button("Modificar"):
+                caixa_up_data = {
+                    "moeda": moeda,
+                    "valor": valor,
+                }
+                try:
+                    put_response = requests.put(
+                        f"http://api.8000/caixa/{caixa_up_data}", json=caixa_up_data
+                    )
+                    show_response_message(put_response)
+                    if put_response.status_code == 200:
+                        st.subheader("Dados inseridos, tudo OK:")
+                    else:
+                        st.subheader("Dados NÃO inseridos, favor revisar:")
+                    show_data_output(caixa_up_data)
+                except Exception as e:
+                    raise e
+        else:
+            show_response_message(update_response)
+
+with tab4:
+    st.header("Deletar Registro de Caixa")
+    delete_id = st.number_input(
+        label="ID Registro", min_value=1, value=None, format="%d"
+    )
+    if delete_id:
+        show_delete_response = requests.get(f"http://api:8000/caixa/{delete_id}")
+        if show_delete_response.status_code == 200:
+            caixa_delete = show_delete_response.json()
+            df_delete = pd.DataFrame([caixa_delete])
+            st.dataframe(df_delete.set_index("id"))
+            delete_confirm = st.checkbox("Confirma que deseja deletar o registro?")
+            delete_button = st.button("Deletar", disabled=(not delete_confirm))
+            if delete_button:
+                delete_response = requests.delete(f"http://api:8000/caixa/{delete_id}")
+                show_response_message(delete_response)
